ec7137142534a34f837fc134ca1ca4833202bd73
Ivan Garcia <ivanvlora@gmail.com>
2018-12-21 15:09:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+#write your code here
+
+def countdown_with_sleep int
+  sleep int
 end
+
+def countdown int
+  while int == int && int > 0
+    int -= 1
+    countdown_with_sleep 1
+    puts "#{int} SECOND(S)!"
+  end
+    "HAPPY NEW YEAR!"
+end
+
